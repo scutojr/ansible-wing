1386931e3591457112c8a36d46692e925506543e
Jayce Au <jayceAu@outlook.com>
2017-05-30 22:21:05 +0800

    feature --- add reload method to Inventory service

--- inventory/service/ivt.py
+++ inventory/service/ivt.py
@@ -1,8 +1,13 @@
+import inventory.model as models
 from inventory.service.base import BasedService
 
 
 class Inventory(BasedService):
     def __init__(self):
+        self._raw_ivt = '{}'
+
+    def reload(self):
+        # TODO: construct the ansible inventory of type json from DB
         pass
 
     def start(self):
