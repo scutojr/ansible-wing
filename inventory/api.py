83c70f5b19ad4c74a512580ce72eeaf30a1f0a1a
Jayce Au <jayceAu@outlook.com>
2017-06-01 20:53:10 +0800

    feature --- add wrapper annotation to view func to unique the response format

--- inventory/api.py
+++ inventory/api.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Blueprint, request
 
 
@@ -6,7 +8,27 @@ __all__ = [
 ]
 
 
+'''
+Convention:
+    1. all the method must has a return value
+'''
+
+
 api = Blueprint('api', __name__)
+
+
+def rsp_json(func):
+    def internal(*args, **kwargs):
+        wrapper = dict()
+        try:
+            rsp = func(*args, **kwargs)
+            wrapper['code'] = 0
+            wrapper['data'] = rsp
+        except Exception as e:
+            wrapper['code'] = 1
+            wrapper['error'] = e.message
+        return json.dumps(wrapper)
+    return internal
 
 
 @api.record_once
@@ -15,30 +37,45 @@ def init():
 
 
 @api.route('/namespace/<namespace>')
+@rsp_json
 def dump_inventory(namespace):
-    pass
+    return True
 
 
 @api.route('/groups/create')
+@rsp_json
 def create_group():
-    pass
+    payload = {
+        'name': 'group_name',
+        'hosts': ['h1', 'h2'],
+        'variables': {
+        },
+        'children': ['g1', 'g2']
+    }
+    name = payload['name']
+    if name == 'all':
+        pass
 
 
 @api.route('/groups/update')
+@rsp_json
 def update_group():
-    pass
+    return True
 
 
 @api.route('/groups/list')
+@rsp_json
 def list_group():
-    pass
+    return True
 
 
 @api.route('/hosts/upsert')
+@rsp_json
 def upsert_hosts():
-    pass
+    return True
 
 
 @api.route('/hosts/get/<host>')
+@rsp_json
 def get_host(host):
-    pass
+    return True
--- inventory/model.py
+++ inventory/model.py
@@ -2,11 +2,13 @@ import mongoengine as me
 
 
 class Host(me.BaseDocument):
+    host = me.StringField(primary_key=True)
     groups = me.ListField()
     variables = me.DynamicEmbeddedDocument()
 
 
 class Group(me.BaseDocument):
+    name = me.StringField(primary_key=True)
     hosts = me.ListField()
     children = me.ListField()
     variables = me.DynamicEmbeddedDocument()
